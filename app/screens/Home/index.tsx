b3f09efe6b59bd1b27a07df57536a2f3eccab72f
azer <azer@azers-MacBook-Pro.local>
2023-02-10 21:20:46 +0400

    image selection

--- app/screens/Home/index.tsx
+++ app/screens/Home/index.tsx
@@ -2,24 +2,23 @@ import { StyleSheet, Text, View } from 'react-native';
 import React, { FC } from 'react';
 import helpers from '../../helpers/helpers';
 import LargeButton from '../../components/LargeButton';
-import { requestCameraPermission } from '../../utils/utils';
+import { requestCameraPermission, selectImage, selectImageFromDevice } from '../../utils/utils';
 
 interface Props { }
 
 const Home: FC<Props> = (): JSX.Element => {
 
-  
-  const handleImageCapture=async():Promise<void>=>{
-    
-    
 
-    if(!helpers.isIOS){   
+  const handleImageCapture = async (): Promise<void> => {
+    if (!helpers.isIOS) {
       await requestCameraPermission()
     }
+    const { path, error } = await selectImage()
 
-   
-      
-   
+  }
+  const handleImageSelection=async():Promise<void>=>{
+
+    const {path, error}=await selectImageFromDevice()
   }
   return (
     <View style={styles.container}>
@@ -32,11 +31,11 @@ const Home: FC<Props> = (): JSX.Element => {
         </Text>
       </View>
       <View style={styles.btnContainer}>
-        <LargeButton text='Capture' icon='camera' onPress={handleImageCapture}/>
-        <LargeButton text='Select' icon='folder'/>
+        <LargeButton text='Capture' icon='camera' onPress={handleImageCapture} />
+        <LargeButton text='Select' icon='folder' onPress={handleImageSelection} />
       </View>
     </View>
-    
+
   )
 }
 
@@ -45,29 +44,29 @@ export default Home;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop:helpers.px(50),
-    paddingHorizontal:helpers.px(16)
+    paddingTop: helpers.px(50),
+    paddingHorizontal: helpers.px(16)
   },
   textContainer: {
-    alignItems:'center',
-    gap:helpers.px(13),
-    flex:1
+    alignItems: 'center',
+    gap: helpers.px(13),
+    flex: 1
   },
   header: {
-    ...helpers.fontStyle("Bold",22)
+    ...helpers.fontStyle("Bold", 22)
   },
   description: {
-    ...helpers.fontStyle("Regular",12),
-    textAlign:'center'
+    ...helpers.fontStyle("Regular", 12),
+    textAlign: 'center'
 
   },
   btnContainer: {
-    flex:1,
-    flexDirection:'row',
-    justifyContent:'center',
-    gap:helpers.px(20)
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    gap: helpers.px(20)
 
 
   },
-  
+
 })
