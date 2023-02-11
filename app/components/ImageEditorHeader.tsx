70be63daeaa4be7421a143537e18c28a1d37e5ed
azer <azer@azers-MacBook-Pro.local>
2023-02-11 23:34:39 +0400

    back button, save button,background image

--- app/components/ImageEditorHeader.tsx
+++ app/components/ImageEditorHeader.tsx
@@ -1,28 +1,30 @@
 import { StyleSheet, TouchableOpacity, View } from 'react-native';
 import React, { FC } from 'react';
-import Icon from 'react-native-vector-icons/AntDesign'
-import colors from '../values/colors';
-import helpers from '../helpers/helpers';
 import UtilityButtons from './UtilityButtons';
+import { NavigationProp, useNavigation } from '@react-navigation/native';
+import { RootStackParamList } from '../Stacks/Stacks';
 
-interface Props{}
 
-const ImageEditorHeader:FC<Props> = ():JSX.Element => {
-  return (
-    <View style={styles.container}>
-        <UtilityButtons.Back/>
-        <UtilityButtons.Save/>
-    </View>
-  )
+interface Props { }
+
+const ImageEditorHeader: FC<Props> = (): JSX.Element => {
+    const navigation=useNavigation<NavigationProp<RootStackParamList>>()
+    return (
+        <View style={styles.container}>
+            
+            <UtilityButtons.Back onPress={navigation.goBack} />
+            <UtilityButtons.Save />
+        </View>
+    )
 }
 
 export default ImageEditorHeader;
 
 const styles = StyleSheet.create({
-  container:{
-    flexDirection:'row',
-    justifyContent:'space-between'
+    container: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
 
-  },
- 
+    },
+
 })
